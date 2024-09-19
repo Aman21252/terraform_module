c2c830104d525334eb4401df68761d1744733645
Aman21252 <amanshaik2300@gmail.com>
2024-09-19 08:32:32 +0530

    Update main.tf

--- module/lambda_functions/main.tf
+++ module/lambda_functions/main.tf
@@ -1,1 +1,61 @@
 
+# Create IAM Role for Lambda
+resource "aws_iam_role" "lambda_role" {
+  name = "${var.lambda_function_name}_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action    = "sts:AssumeRole",
+        Effect    = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ],
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# Write script content to a file locally
+data "local_file" "lambda_script" {
+  content  = var.lambda_script_content
+  filename = "${path.module}/lambda_function.py"
+}
+
+# Create a ZIP file for Lambda
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = data.local_file.lambda_script.filename
+  output_path = "${path.module}/lambda_function.zip"
+}
+
+# Create Lambda Function
+resource "aws_lambda_function" "lambda_function" {
+  function_name = var.lambda_function_name
+  role          = aws_iam_role.lambda_role.arn
+  handler       = var.lambda_handler
+  runtime       = var.lambda_runtime
+
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = filebase64sha256(data.archive_file.lambda_zip.output_path)
+
+  environment {
+    variables = var.lambda_environment_variables
+  }
+
+  timeout     = var.lambda_timeout
+  memory_size = var.lambda_memory
+}
+
+resource "aws_lambda_permission" "lambda_permission" {
+  statement_id  = "AllowExecutionFromAPI"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda_function.function_name
+  principal     = "apigateway.amazonaws.com"
+}
